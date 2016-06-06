636328ca02a96803cab54a8905a2ce35691ba9d7
Dreamcomputer <mallikarjun.s@husky.neu.edu>
2016-06-06 13:22:58 -0400

    Assignment 5: Openshift Deployment

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ app.use(session({
 // Assignmnet 5: DB programming with MongoDB and Mongoose
 var mongoose = require('mongoose');
 var connectionString = 'mongodb://127.0.0.1:27017/FormBuilderApp';
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    console.log("Passwordddddd: " +  process.env.OPENSHIFT_MONGODB_DB_PASSWORD);
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + "/" +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
 var db = mongoose.connect(connectionString);
 var UserModel = require("./public/assignment/server/models/user.model.js")(mongoose, db);
 
